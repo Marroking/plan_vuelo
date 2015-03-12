bd81d76ea21eb979f5c66dbe2be18d1da6f5858b
Miguel Marroquin Estrada <marroquin.em93@gmail.com>
2015-03-12 15:22:02 -0600

    Agregando index y conexion

--- class/conexion.php
+++ class/conexion.php
@@ -1,10 +1,11 @@
 <?php
-	//Crear la conexión a Bi
-	
-	$link = mssql_connect('192.168.100.7', 'sa', 'Radec866')
-		or die('No se puede conectar al servidor');
-	
-	mssql_select_db('PosModulos', $link) or die ('No se peude conectar a la DB');
+  //Crear la conexión a Bi
+  
+  $link = mssql_connect('192.168.100.7', 'sa', 'Radec866')
+    or die('No se puede conectar al servidor');
+  
+  mssql_select_db('PosModulos', $link) or die ('No se peude conectar a la DB');
 
-	
+  
 ?>
+
