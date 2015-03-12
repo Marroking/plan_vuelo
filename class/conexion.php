acda8afa14610b0ee01fbdf96f8c39ca1a2a20c9
Miguel Marroquin Estrada <marroquin.em93@gmail.com>
2015-03-12 15:25:18 -0600

    Update conexion.php

--- class/conexion.php
+++ class/conexion.php
@@ -1,7 +1,7 @@
 <?php
   //Crear la conexión a Bi
   
-  $link = mssql_connect('192.168.100.7', 'sa', 'Radec866')
+  
     or die('No se puede conectar al servidor');
   
   mssql_select_db('PosModulos', $link) or die ('No se peude conectar a la DB');
